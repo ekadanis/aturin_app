d7fcd1238163ec103309f731c814d672a6df2652
tearamizu <yeremiaoctaviano25@gmail.com>
2025-04-28 16:39:54 +0700

    Implementasi animated_splash_screen dengan gif yang dicrop dan memakai ukuran dinamis untuk splashscreen.gif

--- lib/routers/app_router.dart
+++ lib/routers/app_router.dart
@@ -6,17 +6,13 @@ import 'package:aturin_app/features/profile/ui/profile_edit_page.dart';
 import 'package:aturin_app/features/profile/models/user.dart';
 import 'package:aturin_app/Test/task_page.dart';
 import 'package:aturin_app/features/onboarding/ui/onboarding_screen.dart';
-import 'package:aturin_app/features/splash_screen/ui/splash_screen.dart';
+import 'package:aturin_app/features/animated_splash_screen/ui/animated_splash_screen.dart';
 part 'app_router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter{
   @override
   RouteType get defaultRouteType => RouteType.material(
-    //Predictive Back Gesture?? Wowww!!!!
-    //Ini cuma fitur animasi buat android 13+ doang sih, (menurutku keren)
-    //kalau memang mau dipake jangan lupa tambahin
-    //<activity android:enableOnBackInvokedCallback="true" ...> di android manifest
     enablePredictiveBackGesture: true,
   );
 
